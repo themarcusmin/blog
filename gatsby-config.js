4449cb6a952129a1322756681d82b41567665ace
mmo <s3860838@student.rmit.edu.au>
2021-04-08 19:45:52 +0630

    sample mdx & gatsby-config

---
+++ gatsby-config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    siteMetadata: {
+        title: 'my-blog',
+        author: {
+            name: 'minminoo',
+            summary: 'A CS student',
+        },
+        description: 'A blog for my rant',
+        siteUrl: 'https://localhost:8000',
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: ['.mdx', '.md'],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                path: `${__dirname}/posts/`,
+                name: `post`,
+            }
+        },
+    ]
+}
