899f74e5a4ba3c432ba10f33213b24a1fa48d88d
themarcusmin <mmxo5813@gmail.com>
2022-01-07 19:01:13 +0630

    about page

---
+++ pages/about/index.js
@@ -0,0 +1,14 @@
+import Main from "../../components/Main";
+
+export default function about() {
+  return (
+    <Main>
+      <div>I&apos;m Min Min Oo. I like all things digital and writing.</div>
+      <div>So this blog is a perfect excuse for me to write.</div>
+      <div>
+        Here is where I document all sorts of my life experiences. It could be
+        about thoughts, books, travels or even new things I find thrilling.
+      </div>
+    </Main>
+  );
+}
