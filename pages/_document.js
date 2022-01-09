c4078aa667bc6e03444c7dc5bf25cb5446025c06
themarcusmin <mmxo5813@gmail.com>
2022-01-09 16:46:05 +0630

    google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,24 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Global site tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          {/* Google fonts */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           {/* eslint-disable-next-line react/no-unknown-property */}
           <link
