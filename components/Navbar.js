c231eaa0a076d178a74267e6ed3d407fd0ce561a
themarcusmin <mmxo5813@gmail.com>
2022-01-09 15:39:09 +0630

    eslint and link fix

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
 import styles from "../styles/Navbar.module.css";
-import Link from "next/Link";
+import Link from "next/link";
 import { useState, useEffect } from "react";
 
 export default function Navbar() {
--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
 module.exports = {
   reactStrictMode: true,
+  extends: ["eslint:recommended", "next"],
 };
